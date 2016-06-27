8b267907f1c7f0e1f0aa1dc9d57fcf7904be2d3b
admin <shouweiDBS>
2016-06-27 17:04:51 +0800

    add more font icon

--- AuntFood/SecondViewController.swift
+++ AuntFood/SecondViewController.swift
@@ -17,10 +17,15 @@ class SecondViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet var array: [UIImageView]!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         testNetwork()
         testFont()
+        testImageArray()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,12 +36,41 @@ class SecondViewController: UIViewController {
 
     
     //MARK: test
+    func testImageArray(){
+        var cogIcon = FAKFontAwesome.cogIconWithSize(20)
+        cogIcon.addAttribute(NSForegroundColorAttributeName, value: UIColor.blueColor())
+        self.array[0].image = cogIcon.imageWithSize(CGSizeMake(50, 50))
+        
+        cogIcon = FAKFontAwesome.appleIconWithSize(20);
+        self.array[1].image = cogIcon.imageWithSize(CGSizeMake(50, 50))
+        
+        cogIcon = FAKFontAwesome.cropIconWithSize(20);
+        self.array[2].image = cogIcon.imageWithSize(CGSizeMake(50, 50))
+        
+        cogIcon = FAKFontAwesome.eyeIconWithSize(20);
+        self.array[3].image = cogIcon.imageWithSize(CGSizeMake(40,40))
+        
+        
+        var socIcon = FAKZocial.amazonIconWithSize(10)
+        socIcon.addAttribute(NSForegroundColorAttributeName, value: UIColor.blueColor())
+        self.array[4].image = socIcon.imageWithSize(CGSizeMake(40,40))
+        
+        socIcon = FAKZocial.gmailIconWithSize(10);
+        self.array[5].image = socIcon.imageWithSize(CGSizeMake(40,40))
+        
+        socIcon = FAKZocial.googleplayIconWithSize(10);
+        self.array[6].image = socIcon.imageWithSize(CGSizeMake(40,40))
+        
+        socIcon = FAKZocial.facebookIconWithSize(10);
+        self.array[7].image = socIcon.imageWithSize(CGSizeMake(40,40))
+        
+    }
+    
     func  testFont()
     {
-        let cogIcon = FAKFontAwesome.cogIconWithSize(20)
+        let cogIcon = FAKFontAwesome.cogIconWithSize(10)
         cogIcon.addAttribute(NSForegroundColorAttributeName, value: UIColor.blueColor())
         self.imageView.image = cogIcon.imageWithSize(CGSizeMake(20,20))
-        cogIcon.iconFontSize = 15
     }
     
     func testNetwork(){
